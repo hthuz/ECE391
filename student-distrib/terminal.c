eda868b909180ebc20a986a68a09ad0f3af68038
Zhu <hz66@illinois.edu>
2022-10-22 11:31:30 -0500

    add terminal function interfaces

--- student-distrib/terminal.c
+++ student-distrib/terminal.c
@@ -3,4 +3,79 @@
 */
 
 #include "terminal.h"
+#include "lib.h"
+#include "keyboard.h"
 
+
+/* 
+ *
+ *
+ * 
+ */
+int32_t terminal_init()
+{
+
+}
+
+/* terminal_open
+ *   DESCRIPTION: get directory entry to filename
+                  allocate unused file descriptor  
+                  set up necessary data
+ *   INPUTS: filename -- terminal filename
+ *   OUTPUTS: none
+ *   RETURN VALUE: -1 if filename doesn't exist or no descriptors free
+ *                  0 if success
+ *   SIDE EFFECTS: initialize terminal
+ */ 
+int32_t terminal_open(const uint8_t* filename)
+{
+
+}
+
+/* terminal_close
+ *   DESCRIPTION: close fd and make it available
+ *   INPUTS:  fd -- file descriptor  
+ *   OUTPUTS: none
+ *   RETURN VALUE: -1 if try to close invalid fd
+ *                  0 if success
+ *   SIDE EFFECTS: make one fd available
+ */
+int32_t terminal_close(int32_t fd)
+{
+    // if try to close default descriptor
+    if(fd == 0 || fd == 1)
+        return -1;
+}
+
+
+// TODO: to complete the header
+/* terminal_read
+ *   DESCRIPTION: copy from keyboard buffer to buf
+                  only returns when enter key is pressed
+ *   INPUTS:
+ *   OUTPUTS: none
+ *   RETURN VALUE: -1 if failure
+ *                 number of bytes successfully copied if success
+ *   SIDE EFFECTS:
+ */
+int32_t terminal_read(int32_t fd, void* buf, int32_t nbytes)
+{
+
+}
+
+
+/* terminal_write
+ * terminal_write
+ *   DESCRIPTION: print things in buf to screen(write data to terminal)
+ *   INPUTS:  fd -- file descriptor  
+ *            buf -- buffer
+ *            nbytes -- number of bytes to print
+ *   OUTPUTS: none
+ *   RETURN VALUE: -1 if failure
+ *                 number of bytes successfully written/printed if success 
+ *   SIDE EFFECS: 
+ */
+int32_t terminal_write(int32_t fd, const void * buf, int32_t nbytes)
+{
+
+}
--- student-distrib/terminal.h
+++ student-distrib/terminal.h
@@ -1,7 +1,17 @@
+/* termial.h - Defines used in interactions with terminal
+*/
+
+
 #ifndef TERMINAL_H
 #define TERMINAL_H
 
 
+// terminal driver components
+int32_t terminal_init();
+int32_t terminal_open(const uint8_t* filename);
+int32_t terminal_close(int32_t fd);
+int32_t terminal_read(int32_t fd, void* buf, int32_t nbytes);
+int32_t terminal_write(int32_t fd, const void * buf, int32_t nbytes);
 
 
 
