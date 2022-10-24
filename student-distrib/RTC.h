f044ce4e2b9fe8f8f1ebeb2826a923141f0a9c53
xuekunz2 <xuekunz2@illinois.edu>
2022-10-24 09:06:19 -0500

    Replace RTC.h

--- student-distrib/RTC.h
+++ student-distrib/RTC.h
@@ -2,6 +2,8 @@
 #ifndef _RTC_H
 #define _RTC_H
 
+#include "types.h"
+
 #define RTC_PORT  0x70
 #define RTC_DATA  0x71
 
@@ -19,9 +21,19 @@
 
 
 
-extern void rtc_init();
+extern int rtc_init();
 
 extern void rtc_interrupt();
 
+extern int32_t rtc_set_rate(int32_t frequence);
 
+extern int32_t rtc_open(const uint8_t* filename);
+
+extern int32_t rtc_close(int32_t fd);
+
+extern int32_t rtc_read(int32_t fd, void* buf, int32_t nbytes);
+
+extern int32_t rtc_write(int32_t fd, const void * buf, int32_t nbytes);
+
+extern int isPowerOfTwo(int num);
 #endif
